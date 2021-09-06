13cba2452d5543f49d20584079cd58af48eb9616
liuhll <1029765111@qq.com>
2021-09-06 15:03:22 +0800

    更新应用实例

--- src/views/application/instance.vue
+++ src/views/application/instance.vue
@@ -1,50 +1,189 @@
 <template>
   <div class="instance-contanier">
-    <el-button type="text" icon="el-icon-back" @click="goBack()">返回</el-button>
+    <el-button type="text" icon="el-icon-back" @click="goBack()"
+      >返回</el-button
+    >
     <el-descriptions :title="instanceInfo.hostName">
-      <el-descriptions-item label="应用实例地址">{{instanceInfo.address}}</el-descriptions-item>
+      <el-descriptions-item label="实例地址">
+        <el-tag>{{ instanceInfo.address }}</el-tag></el-descriptions-item
+      >
       <el-descriptions-item label="是否健康">
         <el-tag :type="instanceInfo.isHealth === 'true' ? 'success' : 'danger'">
-          {{ instanceInfo.isHealth === 'true' ? "健康" : "不健康" }}
+          {{ instanceInfo.isHealth === "true" ? "健康" : "不健康" }}
         </el-tag>
       </el-descriptions-item>
       <el-descriptions-item label="是否在线">
         <el-tag :type="instanceInfo.isEnable === 'true' ? 'success' : 'danger'">
-          {{ instanceInfo.isEnable === 'true' ? "健康" : "不健康" }}
+          {{ instanceInfo.isEnable === "true" ? "在线" : "离线" }}
         </el-tag>
-      </el-descriptions-item>      
+      </el-descriptions-item>
     </el-descriptions>
     <el-tabs v-model="activeName">
       <el-tab-pane label="本地执行信息" name="first">
         <el-descriptions title="该实例本地执行概要信息">
-          <el-descriptions-item label="首次执行本地方法时间">{{dateFormat(instanceHandleInfo.firstHandleTime,"yyyy-mm-dd HH:MM:ss")}}</el-descriptions-item>
-          <el-descriptions-item label="最后执行本地方法时间">{{dateFormat(instanceHandleInfo.finalHandleTime, "yyyy-mm-dd HH:MM:ss")}}</el-descriptions-item>
-          <el-descriptions-item label="平均执行时长">{{displayAet(instanceHandleInfo.aet)}} ms</el-descriptions-item>
-          <el-descriptions-item label="本地执行次数(汇总)">{{instanceHandleInfo.totalHandles}}</el-descriptions-item>
-          <el-descriptions-item label="本地执行异常次数">{{instanceHandleInfo.faultHandles}}</el-descriptions-item>
-          <el-descriptions-item label="本地执行严重错误(非业务异常)次数">{{instanceHandleInfo.seriousError}}</el-descriptions-item>
-          <el-descriptions-item label="最后发生严重错误(非业务异常)时间" v-if="instanceHandleInfo.finalSeriousErrorTime">{{dateFormat(instanceHandleInfo.finalSeriousErrorTime,"yyyy-mm-dd HH:MM:ss")}}</el-descriptions-item>
+          <el-descriptions-item label="首次执行本地方法时间">{{
+            displayDate(instanceHandleInfo.firstHandleTime)
+          }}</el-descriptions-item>
+          <el-descriptions-item label="最后执行本地方法时间">{{
+            displayDate(instanceHandleInfo.finalHandleTime)
+          }}</el-descriptions-item>
+          <el-descriptions-item label="平均执行时长"
+            >{{ displayAet(instanceHandleInfo.aet) }} ms</el-descriptions-item
+          >
+          <el-descriptions-item label="最大并发量"
+            >{{ instanceHandleInfo.maxConcurrentHandles }}
+          </el-descriptions-item>
+          <el-descriptions-item label="执行次数(汇总)"
+            >{{ instanceHandleInfo.totalHandles }}次</el-descriptions-item
+          >
+          <el-descriptions-item label="异常执行次数"
+            >{{ instanceHandleInfo.faultHandles }}次</el-descriptions-item
+          >
+          <el-descriptions-item label="严重错误(非业务异常)"
+            >{{ instanceHandleInfo.totalSeriousError }}次</el-descriptions-item
+          >
+          <el-descriptions-item
+            label="最后发生严重错误(非业务异常)时间"
+            v-if="instanceHandleInfo.finalSeriousErrorTime"
+            >{{
+              displayDate(instanceHandleInfo.finalSeriousErrorTime)
+            }}</el-descriptions-item
+          >
         </el-descriptions>
+        <el-table
+          :data="serviceHandles.items"
+          :span-method="objectSpanHandleMethod"
+        >
+          <el-table-column
+            label="服务条目"
+            prop="serviceId"
+            width="400"
+          ></el-table-column>
+          <el-table-column label="消费者地址">
+            <template #default="scope">
+              <el-tag>{{ scope.row.address }} </el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column label="首次执行时间">
+            <template #default="scope">
+              {{ displayDate(scope.row.serviceHandleInfo.firstHandleTime) }}
+            </template>
+          </el-table-column>
+          <el-table-column label="最后执行时间">
+            <template #default="scope">
+              {{ displayDate(scope.row.serviceHandleInfo.finalHandleTime) }}
+            </template>
+          </el-table-column>
+          <el-table-column label="平均执行时长(ms)">
+            <template #default="scope">
+              {{ displayAet(scope.row.serviceHandleInfo.aet) }}
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="总执行次数(次)"
+            prop="serviceHandleInfo.totalHandles"
+          ></el-table-column>
+          <el-table-column
+            label="异常执行次数(次)"
+            prop="serviceHandleInfo.faultHandles"
+          ></el-table-column>
+          <el-table-column
+            label="严重错误[非业务异常](次)"
+            prop="serviceHandleInfo.seriousError"
+          ></el-table-column>
+          <el-table-column label="最后严重错误[非业务异常]发生时间">
+            <template #default="scope">
+              {{ displayDate(scope.row.serviceHandleInfo.seriousErrorTime) }}
+            </template>
+          </el-table-column>
+        </el-table>
       </el-tab-pane>
       <el-tab-pane label="Rpc请求信息" name="second">
         <el-descriptions title="该实例Rpc请求概要信息">
-          <el-descriptions-item label="首次rpc请求时间">{{dateFormat(instanceInvokeInfo.firstInvokeTime,"yyyy-mm-dd HH:MM:ss")}}</el-descriptions-item>
-          <el-descriptions-item label="最后rpc请求时间">{{dateFormat(instanceInvokeInfo.finalInvokeTime, "yyyy-mm-dd HH:MM:ss")}}</el-descriptions-item>
-          <el-descriptions-item label="rpc平均请求执行时长">{{displayAet(instanceInvokeInfo.aet)}} ms</el-descriptions-item>
-          <el-descriptions-item label="rpc请求次数(汇总)">{{instanceInvokeInfo.totalRequests}}</el-descriptions-item>
-          <el-descriptions-item label="rpc请求异常次数">{{instanceInvokeInfo.faultRequests}}</el-descriptions-item>
-          <el-descriptions-item label="最后请求异常时间" v-if="instanceInvokeInfo.finalFaultInvokeTime">{{dateFormat(instanceInvokeInfo.finalFaultInvokeTime,"yyyy-mm-dd HH:MM:ss")}}</el-descriptions-item>
-        </el-descriptions> 
+          <el-descriptions-item label="首次rpc请求时间">{{
+            displayDate(instanceInvokeInfo.firstInvokeTime)
+          }}</el-descriptions-item>
+          <el-descriptions-item label="最后rpc请求时间">{{
+            displayDate(instanceInvokeInfo.finalInvokeTime)
+          }}</el-descriptions-item>
+          <el-descriptions-item label="rpc平均请求响应时长"
+            >{{ displayAet(instanceInvokeInfo.aet) }} ms</el-descriptions-item
+          >
+          <el-descriptions-item label="最大并发量"
+            >{{ displayAet(instanceInvokeInfo.maxConcurrentRequests) }}
+          </el-descriptions-item>
+          <el-descriptions-item label="rpc请求次数(汇总)">{{
+            instanceInvokeInfo.totalRequests
+          }}</el-descriptions-item>
+          <el-descriptions-item label="rpc请求异常次数">{{
+            instanceInvokeInfo.faultRequests
+          }}</el-descriptions-item>
+          <el-descriptions-item
+            label="最后请求异常时间"
+            v-if="instanceInvokeInfo.finalFaultInvokeTime"
+            >{{
+              displayDate(instanceInvokeInfo.finalFaultInvokeTime)
+            }}</el-descriptions-item
+          >
+        </el-descriptions>
+        <el-table
+          :data="serviceInvokes.items"
+          :span-method="objectSpanInvokeMethod"
+        >
+          <el-table-column
+            label="服务条目"
+            prop="serviceId"
+            width="400"
+          ></el-table-column>
+          <el-table-column label="服务提供者地址(当前状态)">
+            <template #default="scope">
+              <el-tag :type="scope.row.isEnable ? 'success' : 'danger'"
+                >{{ scope.row.address }}
+                {{ scope.row.isEnable ? "(在线)" : "(离线)" }}</el-tag
+              >
+            </template>
+          </el-table-column>
+          <el-table-column label="首次调用时间">
+            <template #default="scope">
+              {{ displayDate(scope.row.serviceInvokeInfo.firstInvokeTime) }}
+            </template>
+          </el-table-column>
+          <el-table-column label="最后调用时间">
+            <template #default="scope">
+              {{ displayDate(scope.row.serviceInvokeInfo.finalInvokeTime) }}
+            </template>
+          </el-table-column>
+          <el-table-column label="平均响应(ms)">
+            <template #default="scope">
+              {{ displayAet(scope.row.serviceInvokeInfo.aet) }}
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="总请求(次)"
+            prop="serviceInvokeInfo.totalRequests"
+          ></el-table-column>
+          <el-table-column
+            label="异常请求(次)"
+            prop="serviceInvokeInfo.faultRequests"
+          ></el-table-column>
+          <el-table-column label="最后异常请求时间">
+            <template #default="scope">
+              {{
+                displayDate(scope.row.serviceInvokeInfo.finalFaultInvokeTime)
+              }}
+            </template>
+          </el-table-column>
+        </el-table>
       </el-tab-pane>
     </el-tabs>
   </div>
 </template>
 
 <script lang="ts">
-import { ref, onMounted } from 'vue';
+import { ref, onMounted } from "vue";
 import dateFormat from "dateformat";
 import { useRoute, useRouter } from "vue-router";
-import { storageLocal } from "/@/utils/storage"
+import { storageLocal } from "/@/utils/storage";
 import { useApplicationStoreHook } from "/@/store/modules/applications";
 export default {
   setup() {
@@ -55,20 +194,24 @@ export default {
     let instanceInfo = ref({});
     let instanceHandleInfo = ref({});
     let instanceInvokeInfo = ref({});
-
     let serviceHandlePageRequest = ref({
       pageIndex: 1,
       pageSize: 10
     });
-
     let serviceInvokePageRequest = ref({
       pageIndex: 1,
       pageSize: 10
     });
 
+    let serviceHandles = ref({});
+    let serviceInvokes = ref({});
+
+    let spanHandleArr = [];
+    let spanInvokeArr = [];
+
     const goBack = () => {
       router.go(-1);
-    }
+    };
 
     const getInstanceDetail = () => {
       const address = storageLocal.getItem("instanceAddress");
@@ -78,61 +221,137 @@ export default {
           instanceInvokeInfo.value = data["invokeInfo"];
         });
       }
-    }
-    
+    };
+
     const getInstanceServiceHandle = () => {
       const address = storageLocal.getItem("instanceAddress");
       if (address) {
-        applicationStore.getInstanceServiceHandle(address, serviceHandlePageRequest.value).then(data => {
-          instanceHandleInfo.value = data["handleInfo"];
-          instanceInvokeInfo.value = data["invokeInfo"];
-        });
+        applicationStore
+          .getInstanceServiceHandle(address, serviceHandlePageRequest.value)
+          .then(data => {
+            serviceHandles.value = data;
+            getSpanHandleArr(data["items"]);
+          });
       }
-    }
+    };
 
     const getInstanceServiceInvoke = () => {
       const address = storageLocal.getItem("instanceAddress");
       if (address) {
-        applicationStore.getInstanceServiceInvoke(address, serviceInvokePageRequest.value).then(data => {
-          instanceHandleInfo.value = data["handleInfo"];
-          instanceInvokeInfo.value = data["invokeInfo"];
-        });
-      } 
-    }
-    
-    const displayAet = (aet) => {
+        applicationStore
+          .getInstanceServiceInvoke(address, serviceInvokePageRequest.value)
+          .then(data => {
+            serviceInvokes.value = data;
+            getSpanInvokeArr(data["items"]);
+          });
+      }
+    };
+
+    const displayAet = aet => {
       if (aet) {
-        aet.toFixed(2);
+        return aet.toFixed(2);
       }
       return 0;
-    }
+    };
+
+    const displayDate = date => {
+      if (date) {
+        return dateFormat(date, "yyyy-mm-dd HH:MM:ss");
+      }
+      return "";
+    };
 
     onMounted(() => {
       const address = route.query["address"];
       if (address) {
-        storageLocal.setItem('instanceAddress', address);
+        storageLocal.setItem("instanceAddress", address);
       }
-      if(Object.keys(route.params).length > 0) {
-        storageLocal.setItem("instance",route.params);
+      if (Object.keys(route.params).length > 0) {
+        storageLocal.setItem("instance", route.params);
       }
       instanceInfo.value = storageLocal.getItem("instance");
-
-      getInstanceDetail();
       getInstanceServiceHandle();
       getInstanceServiceInvoke();
+      getInstanceDetail();
     });
+
+    const getSpanHandleArr = (data: any[]) => {
+      spanHandleArr = [];
+      let pos = 0;
+      for (let i = 0; i < data.length; i++) {
+        if (i === 0) {
+          spanHandleArr.push(1);
+          pos = 0;
+        } else {
+          // 判断当前元素与上一个元素是否相同
+          if (data[i].serviceId === data[i - 1].serviceId) {
+            spanHandleArr[pos] += 1;
+            spanHandleArr.push(0);
+          } else {
+            spanHandleArr.push(1);
+            pos = i;
+          }
+        }
+      }
+    };
+
+    const objectSpanHandleMethod = ({ row, column, rowIndex, columnIndex }) => {
+      if (columnIndex == 0) {
+        const _row = spanHandleArr[rowIndex];
+        const _col = _row > 0 ? 1 : 0;
+        return {
+          rowspan: _row,
+          colspan: _col
+        };
+      }
+    };
+
+    const getSpanInvokeArr = (data: any[]) => {
+      spanInvokeArr = [];
+      let pos = 0;
+      for (let i = 0; i < data.length; i++) {
+        if (i === 0) {
+          spanInvokeArr.push(1);
+          pos = 0;
+        } else {
+          // 判断当前元素与上一个元素是否相同
+          if (data[i].serviceId === data[i - 1].serviceId) {
+            spanInvokeArr[pos] += 1;
+            spanInvokeArr.push(0);
+          } else {
+            spanInvokeArr.push(1);
+            pos = i;
+          }
+        }
+      }
+    };
+
+    const objectSpanInvokeMethod = ({ row, column, rowIndex, columnIndex }) => {
+      if (columnIndex == 0) {
+        const _row = spanHandleArr[rowIndex];
+        const _col = _row > 0 ? 1 : 0;
+        return {
+          rowspan: _row,
+          colspan: _col
+        };
+      }
+    };
 
     return {
       instanceInfo,
       instanceHandleInfo,
       instanceInvokeInfo,
       activeName,
+      serviceHandles,
+      serviceInvokes,
       goBack,
-      dateFormat,
-      displayAet
-    }
+      displayDate,
+      displayAet,
+      objectSpanHandleMethod,
+      objectSpanInvokeMethod
+    };
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
