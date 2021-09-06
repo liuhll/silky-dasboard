36dde23a1ad996ab48a7784880abab58939c7621
liuhll <1029765111@qq.com>
2021-09-06 17:51:56 +0800

    优化应用名称命名

--- src/views/application/index.vue
+++ src/views/application/index.vue
@@ -16,12 +16,18 @@
             {{ item.hostName }}
           </template>
           <el-descriptions :column="1">
-            <el-descriptions-item label="主机实例">{{ item.instanceCount }}</el-descriptions-item>
-            <el-descriptions-item label="应用服务">{{ item.appServiceCount }}</el-descriptions-item>
-            <el-descriptions-item label="服务条目">{{ item.serviceEntriesCount }}</el-descriptions-item>
+            <el-descriptions-item label="应用实例">{{
+              item.instanceCount
+            }}</el-descriptions-item>
+            <el-descriptions-item label="应用服务">{{
+              item.appServiceCount
+            }}</el-descriptions-item>
+            <el-descriptions-item label="服务条目">{{
+              item.serviceEntriesCount
+            }}</el-descriptions-item>
             <el-descriptions-item label="是否支持WebSocket">
               <el-tag :type="item.hasWsService ? 'success' : 'danger'">
-              {{ item.hasWsService ? "支持" : "不支持" }}
+                {{ item.hasWsService ? "支持" : "不支持" }}
               </el-tag>
             </el-descriptions-item>
           </el-descriptions>
@@ -34,12 +40,16 @@
           <el-tab-pane label="微服务实例" name="first">
             <el-table :data="applicationInstances.items">
               <el-table-column
-                label="主机名称"
+                label="应用名称"
                 prop="hostName"
               ></el-table-column>
               <el-table-column label="地址">
                 <template #default="scope">
-                  <el-button type="text" @click="handleSelectedInstance(scope.row)">{{scope.row.address}}</el-button>
+                  <el-button
+                    type="text"
+                    @click="handleSelectedInstance(scope.row)"
+                    >{{ scope.row.address }}</el-button
+                  >
                 </template>
               </el-table-column>
               <el-table-column label="健康状态">
@@ -81,12 +91,13 @@
                   prop="appService"
                   width="320"
                 ></el-table-column>
-                <el-table-column
-                  label="服务条目Id"
-                  width="450"
-                >
+                <el-table-column label="服务条目Id" width="450">
                   <template #default="scope">
-                    <el-button type="text" @click="handleSelectServiceEntry(scope.row)">{{scope.row.serviceId}}</el-button>
+                    <el-button
+                      type="text"
+                      @click="handleSelectServiceEntry(scope.row)"
+                      >{{ scope.row.serviceId }}</el-button
+                    >
                   </template>
                 </el-table-column>
                 <el-table-column
@@ -260,16 +271,26 @@ export default {
     const displayHttpMethod = (httpMethod: Number) => {
       return HttpMethod[httpMethod];
     };
-    const handleSelectedInstance = (row) => {
-      router.push({ name: 'instance', query: { address: row.address }, params: { address: row.address, hostName: row.hostName, isHealth: row.isHealth, isEnable: row.isEnable }})
+    const handleSelectedInstance = row => {
+      router.push({
+        name: "instance",
+        query: { address: row.address },
+        params: {
+          address: row.address,
+          hostName: row.hostName,
+          isHealth: row.isHealth,
+          isEnable: row.isEnable
+        }
+      });
     };
-    const handleSelectServiceEntry = (row) => {
-      router.push( { name: 'serviceentry', query: { serviceEntryId: row.serviceId }});
+    const handleSelectServiceEntry = row => {
+      router.push({
+        name: "serviceentry",
+        query: { serviceEntryId: row.serviceId }
+      });
     };
 
-    const handleClick = (tab, event)=>{
-
-    }
+    const handleClick = (tab, event) => {};
 
     return {
       applications,
